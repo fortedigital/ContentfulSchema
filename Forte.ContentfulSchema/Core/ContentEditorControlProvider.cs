2cb4dd375f942e790d60fb4bdbb82f53adc239a9
Konrad Lalik <konrad.lalik@fortedigital.no>
2017-09-14 09:39:51 +0200

    Add generic providers for field type and field editor

--- Forte.ContentfulSchema/Core/ContentEditorControlProvider.cs
+++ Forte.ContentfulSchema/Core/ContentEditorControlProvider.cs
@@ -8,19 +8,19 @@ using Forte.ContentfulSchema.ContentTypes;
 
 namespace Forte.ContentfulSchema.Core
 {
-    class GenericContentEditorControlProvider : IContentEditorControlProvider
+    public class ContentEditorControlProvider : IContentEditorControlProvider
     {
         private List<(Func<PropertyInfo, Field, bool> Predicate, string Control)> _controlsMap = new List<(Func<PropertyInfo, Field, bool> Predicate, string Control)>();
 
-        public GenericContentEditorControlProvider()
+        public ContentEditorControlProvider()
         {
             AddRule((prop, field) => field.Id == "slug", SystemWidgetIds.SlugEditor);
             AddRule((prop, field) => prop.PropertyType.IsAssignableFrom(typeof(ILongString)), SystemWidgetIds.MultipleLine);
         }
 
-        public List<(Func<PropertyInfo, Field, bool> Predicate, string Control)> ControlsMap => throw new NotImplementedException();
+        public List<(Func<PropertyInfo, Field, bool> Predicate, string Control)> ControlsMap => _controlsMap;
 
-        private void AddRule(Func<PropertyInfo, Field, bool> predicate, string widgetId)
+        public void AddRule(Func<PropertyInfo, Field, bool> predicate, string widgetId)
         {
             _controlsMap.Add((Predicate: predicate, Control: widgetId ));
         }
---
+++ Forte.ContentfulSchema/Core/ContentFieldTypeProvider.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Linq;
+using Contentful.Core.Models.Management;
+using Contentful.Core.Models;
+using Forte.ContentfulSchema.ContentTypes;
+
+namespace Forte.ContentfulSchema.Core
+{
+    public class ContentFieldTypeProvider : IContentFieldTypeProvider
+    {
+        private List<(Func<PropertyInfo, bool> Predicate, string ContentfulType)> _typeRules;
+
+        public ContentFieldTypeProvider()
+        {
+            AddRule(prop => prop.PropertyType == typeof(string), SystemFieldTypes.Symbol);
+            AddRule(prop => prop.PropertyType == typeof(bool), SystemFieldTypes.Boolean);
+            AddRule(prop => prop.PropertyType == typeof(DateTime), SystemFieldTypes.Date);
+            AddRule(prop => prop.PropertyType == typeof(int) || prop.PropertyType == typeof(long), SystemFieldTypes.Integer);
+            AddRule(prop => prop.PropertyType == typeof(float) || prop.PropertyType == typeof(double), SystemFieldTypes.Number);
+            AddRule(prop => prop.PropertyType == typeof(Asset), SystemFieldTypes.Link);
+            AddRule(prop => prop.PropertyType.IsConstructedGenericType && 
+                            prop.PropertyType.GetGenericTypeDefinition() == typeof(Entry<>) , SystemFieldTypes.Link);
+            AddRule(prop => typeof(Entry).IsAssignableFrom(prop.PropertyType), SystemFieldTypes.Link);
+            AddRule(prop => prop.PropertyType == typeof(Location), SystemFieldTypes.Location);
+            AddRule(prop => prop.PropertyType.IsConstructedGenericType &&
+                            typeof(IEnumerable<>).MakeGenericType(prop.PropertyType.GetGenericArguments()[0])
+                                .IsAssignableFrom(prop.PropertyType), SystemFieldTypes.Array);
+        }
+
+        public string GetContentfulTypeForProperty(PropertyInfo property)
+        {
+            var registeredType = _typeRules.SingleOrDefault(rule => rule.Predicate(property));
+
+            if (!string.IsNullOrEmpty(registeredType.ContentfulType))
+            {
+                return registeredType.ContentfulType;
+            }
+
+            return SystemFieldTypes.Symbol;
+        }
+
+        public void AddRule(Func<PropertyInfo, bool> predicate, string contentfulType)
+        {
+            _typeRules.Add((predicate, contentfulType));
+        }
+    }
+}
